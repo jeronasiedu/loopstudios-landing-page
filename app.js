eaee72d9ab59981f7ea20c9f63975c3a5275b4a8
Jeron <norejudeisa@gmail.com>
2022-04-26 16:15:04 -0700

    removed scrolling when mobile menu is opened

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const mobileNav = document.querySelector('.mobile')
 menu.addEventListener('click', () => {
   menu.classList.toggle('active')
   mobileNav.classList.toggle('expand')
+  document.body.style.overflowY =
+    document.body.style.overflowY === 'hidden' ? 'visible' : 'hidden'
 })
